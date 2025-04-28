6c8c05113915472631d0393b7de749cca3a812d5
SamJDriver <SamJDriv@gmail.com>
2025-04-27 19:25:20 -0700

    Added initial nav bar

---
+++ alarm_shock/lib/pages/alarm list page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _AlarmListPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Alarm List'),
+      ),
+      body: const Center(
+        child: Text('Alarm List'),
+      ),
+    );
+  }
+}
